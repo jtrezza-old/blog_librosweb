62f85292eb81f550f1a2bd1915545e82c1535395
Jose Trezza <jtrezza@MacBook-Pro-de-Jose.local>
2015-02-08 19:58:14 -0500

    Comments funcionality is done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208000815) do
+ActiveRecord::Schema.define(version: 20150209000726) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 20150208000815) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter",  limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "article_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  add_foreign_key "comments", "articles"
 end
